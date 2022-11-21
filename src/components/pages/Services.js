286349ee3fb4bea86e8565941807bab9144e4a30
yuripelichek <57309738+yuripelichek@users.noreply.github.com>
2022-11-21 04:17:24 -0300

    Ajuste layout

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -8,7 +8,7 @@ export default function Services() {
 	<div class="row">
 		<div class="col-md-8">
 			<h1>
-				Heading
+				Reciclagem do condomínio
 			</h1>
 
       <ul class="list-group">
@@ -28,25 +28,61 @@ export default function Services() {
       <p> </p><p> </p><p> </p><p> </p>
       <h5>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Para evitarmos esse tipo de processo preparamos algumas dicas.</h5>
         <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&#9642; 1º  Jogue a comida no lixo. Sempre que descartar seu reciclável, lembre-se de limpa-ló para que seja reciclado de forma correta.</p>
-
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src='/images/img01.jpg'  />
+        <p> </p>
         <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&#9642; 2° Quando for jogar o seu lixo, fique atento a cor das lixeiras, a marrom sendo a normal e a verde a reciclável.</p>
-
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src='/images/img02.jpg'  />
+        <p> </p>
         <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&#9642; 3° Na separação dos recicláveis não misture o vidro com os outros, guarde os vidros ou coloque em um pote de leite, para evitar que machuque as pessoas que recolhem o lixo.</p>
-
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src='/images/img03.jpg'  />
+        <p> </p>
         <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&#9642; 4° Lave bem o que for reciclar, muitos desses recicláveis estão indo com comida, mofando e efetivamente estragando outros lixos, impedindo de ser reciclado.</p>
-
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src='/images/img04.jpg'  />
+        <p> </p>
         <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&#9642; 5 ° Muitos estão descartando as garrafas com óleo, não está errado, porém é necessário colocar separado dos demais lixos, colocar logo abaixo da lixeira de recicláveis.</p>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src='/images/img05.jpg'  />
 		</div>
 		<div class="col-md-4">
 			<h1>
-				Heading
+				Horários
 			</h1>
-			<p>
-				Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-			</p>
-			<p>
-				<a class="btn" href="#">View details »</a>
-			</p>
+      <div class="row">
+        <div class="col-md-2">
+        Comum
+        </div>
+        <div class="col-md-2">seg 15h</div>
+      </div>			
+      <div class="row">
+        <div class="col-md-2">
+        </div>
+        <div class="col-md-2">qua 15h</div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-2">
+        </div>
+        <div class="col-md-2">sex 15h</div>
+      </div>
+      <p></p>
+      <div class="row">
+        <div class="col-md-2">
+        Reciclável
+        </div>
+        <div class="col-md-2">seg 17h</div>
+      </div>			
+      <div class="row">
+        <div class="col-md-2">
+        </div>
+        <div class="col-md-2">qui 17h</div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-2">
+        </div>
+        <div class="col-md-2">sáb 08h</div>
+      </div>
+			
+			
 		</div>
 	</div>
 </div>
